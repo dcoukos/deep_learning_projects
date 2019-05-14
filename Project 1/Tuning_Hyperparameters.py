f86011a6e02eb155521c01e177b21b8d404cefba
Dave <niels.mortensen@epfl.ch>
2019-05-14 23:01:36 +0200

    iterator on auxiliarryloss coefficient done

--- Project 1/Tuning_Hyperparameters.py
+++ Project 1/Tuning_Hyperparameters.py
@@ -57,21 +57,21 @@ printing =True
 AuxilaryLoss=0.1
 # Auxiliarryloss :
 AuxilaryLoss=0.1
-AuxilaryLoss_min=0.05
+AuxilaryLoss_min=0
 AuxilaryLoss_max=0.45
 n_AuxilaryLoss=10
 
 # Declaration of the model : 
 model=Whole_Shared_Net()
 
-# Tuning of the Full net with Weightsharing with Auxilarry Loss: 
-print('Tuning for lr. Training the Full net with Weightsharing and auxilarry Loss= {:0.2f} with varying lr :'.format(AuxilaryLoss))
-for lr in np.linspace(lr_min, lr_max, n_lr):
-    model=Whole_Shared_Net()
-    print('Training with lr= {:0.5f}'.format(lr))
-    # Format : def train_model(model, train_input, train_target, test_input, test_target, batch_size=100, epochs=150, lr = 0.01, printing = True, full = False, auxiliaryLoss = 0.2)
-    train_model(model, train_images, (train_digit1, train_digit2, train_comparison), val_images, (val_digit1, val_digit2, val_comparison), batch_size, epochs, lr, printing , True, AuxilaryLoss) # So we use the validation set to set the hyperparameters.
-    # Cnclusion : lr=0.0060 is the best learning rate, but not by far. It achieves 13.00% error rate on comparison on the test set. With this lr, I try several AuxilaryLoss values : 
+## Tuning of the Full net with Weightsharing with Auxilarry Loss: 
+#print('Tuning for lr. Training the Full net with Weightsharing and auxilarry Loss= {:0.2f} with varying lr :'.format(AuxilaryLoss))
+#for lr in np.linspace(lr_min, lr_max, n_lr):
+#    model=Whole_Shared_Net()
+#    print('Training with lr= {:0.5f}'.format(lr))
+#    # Format : def train_model(model, train_input, train_target, test_input, test_target, batch_size=100, epochs=150, lr = 0.01, printing = True, full = False, auxiliaryLoss = 0.2)
+#    train_model(model, train_images, (train_digit1, train_digit2, train_comparison), val_images, (val_digit1, val_digit2, val_comparison), batch_size, epochs, lr, printing , True, AuxilaryLoss) # So we use the validation set to set the hyperparameters.
+#    # Cnclusion : lr=0.0060 is the best learning rate, but not by far. It achieves 13.00% error rate on comparison on the test set. With this lr, I try several AuxilaryLoss values : 
     
 lr=0.006
 print('Tuning for auxiliary loss coefficient. Training the Full net with Weightsharing and lr = {:0.4f} with varying auxilarry Loss :'.format(lr))
